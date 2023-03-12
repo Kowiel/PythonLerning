e269f32f57a001b998db9dd6ccca48ed299d3714
Kowiel <86078928+Kowiel@users.noreply.github.com>
2023-03-12 11:38:48 +0100

    Cleaning up in the code
    Cleaning up the code and making it more redable.

--- Python/PRogram.py
+++ Python/PRogram.py
@@ -2,11 +2,13 @@ import time
 
 
 def ReadFile(filename):
+    """Reads a file and returns a list of tasks that are in the file"""
     with open(f"{filename}","r") as file:
             todo=file.readlines()
     return todo
 
 def WriteFile(filename , data):
+       """Write to the file the data that you provided"""
        with open(f"{filename}","w") as file:
             file.writelines(data)
 
